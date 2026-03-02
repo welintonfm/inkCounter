38fbdf3d4a27a44a86f2fd482ee0f56c2bc6513e
Welinton Faria <welintonfm@gmail.com>
2026-03-02 19:47:29 -0300

    [ADD] Life up and down

--- my-app/src/components/PlayerCard.js
+++ my-app/src/components/PlayerCard.js
@@ -7,7 +7,6 @@ const PlayerCard = ({number, startLife, invert}) => {
 
     const handleChangeLife = (ammount) => {
         let currentLife = life + ammount
-        console.log(currentLife)
         setLife(currentLife)
         return
     }
@@ -16,15 +15,18 @@ const PlayerCard = ({number, startLife, invert}) => {
         <div style={{height: "100%", width: "100%", display: "flex", flexDirection: "column", padding: "32px", alignItems: "center", rotate: invert ? "180deg" : "0"}}>
             <h2 style={{color: "black", fontSize: 64, marginTop: '32px'}}>Player {number}</h2>
             <div style={{backgroundColor: "white", width: "100%", display: "flex", gap: "96px", alignItems: "center", justifyContent: "center"}}>
-                <button style={{backgroundColor: 'transparent', fontSize: 124, color: "black", fontWeight: "bold", border: "none"}}>-</button>
+                <button onClick={() => handleChangeLife(-1)} style={{backgroundColor: 'transparent', height: "80%", width: "200px", fontSize: 124, color: "black", fontWeight: "bold", border: "none"}}>-</button>
                 <p style={{color: "black", fontSize: 350, fontWeight: "bold"}}>{life}</p>
-                <button style={{backgroundColor: 'transparent', fontSize: 124, color: "black", fontWeight: "bold", border: "none"}}>+</button>
+                <button onClick={() => handleChangeLife(1)} style={{backgroundColor: 'transparent', height: "80%", width: "200px", fontSize: 124, color: "black", fontWeight: "bold", border: "none"}}>+</button>
             </div>
             <div style={{display: "flex", gap: "16px", width: "100%", justifyContent: "center", marginTop: "-32px"}}>
                 <button onClick={() => handleChangeLife(-10)} style={{backgroundColor: 'transparent', fontSize: 48, color: "black", border: "3px solid black", padding: "8px", borderRadius: "8px", width: "120px"}}>-10</button>
                 <button onClick={() => handleChangeLife(-5)} style={{backgroundColor: 'transparent', fontSize: 48, color: "black", border: "3px solid black", padding: "8px", borderRadius: "8px", width: "120px"}}>-5</button>
                 <button onClick={() => handleChangeLife(5)} style={{backgroundColor: 'transparent', fontSize: 48, color: "black", border: "3px solid black", padding: "8px", borderRadius: "8px", width: "120px"}}>+5</button>
                 <button onClick={() => handleChangeLife(10)} style={{backgroundColor: 'transparent', fontSize: 48, color: "black", border: "3px solid black", padding: "8px", borderRadius: "8px", width: "120px"}}>+10</button>
+            </div>
+            <div>
+
             </div>
         </div>
     )
