da72029d8325b446d7fb8684b659d18a4f0e16ca
Welinton Faria <welintonfm@gmail.com>
2026-03-02 19:45:28 -0300

    first commit

---
+++ my-app/src/app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import PlayerCard from "@/components/PlayerCard";
+
+export default function Home() {
+  return (
+    <div style={{backgroundColor: "white", height: "100vh", minWidth: "100%", display: "flex", flexDirection: "column"}}>
+     <PlayerCard number="2" startLife={20} invert/>
+     <div style={{backgroundColor: "black", width: "100%", height: "16px"}} />
+     <PlayerCard number="1" startLife={20}/>
+    </div>
+  );
+}
---
+++ my-app/src/components/PlayerCard.js
@@ -0,0 +1,33 @@
+'use client'
+import React from 'react'
+import { useState } from 'react'
+
+const PlayerCard = ({number, startLife, invert}) => {
+    const [life, setLife] = useState(startLife)
+
+    const handleChangeLife = (ammount) => {
+        let currentLife = life + ammount
+        console.log(currentLife)
+        setLife(currentLife)
+        return
+    }
+
+    return (
+        <div style={{height: "100%", width: "100%", display: "flex", flexDirection: "column", padding: "32px", alignItems: "center", rotate: invert ? "180deg" : "0"}}>
+            <h2 style={{color: "black", fontSize: 64, marginTop: '32px'}}>Player {number}</h2>
+            <div style={{backgroundColor: "white", width: "100%", display: "flex", gap: "96px", alignItems: "center", justifyContent: "center"}}>
+                <button style={{backgroundColor: 'transparent', fontSize: 124, color: "black", fontWeight: "bold", border: "none"}}>-</button>
+                <p style={{color: "black", fontSize: 350, fontWeight: "bold"}}>{life}</p>
+                <button style={{backgroundColor: 'transparent', fontSize: 124, color: "black", fontWeight: "bold", border: "none"}}>+</button>
+            </div>
+            <div style={{display: "flex", gap: "16px", width: "100%", justifyContent: "center", marginTop: "-32px"}}>
+                <button onClick={() => handleChangeLife(-10)} style={{backgroundColor: 'transparent', fontSize: 48, color: "black", border: "3px solid black", padding: "8px", borderRadius: "8px", width: "120px"}}>-10</button>
+                <button onClick={() => handleChangeLife(-5)} style={{backgroundColor: 'transparent', fontSize: 48, color: "black", border: "3px solid black", padding: "8px", borderRadius: "8px", width: "120px"}}>-5</button>
+                <button onClick={() => handleChangeLife(5)} style={{backgroundColor: 'transparent', fontSize: 48, color: "black", border: "3px solid black", padding: "8px", borderRadius: "8px", width: "120px"}}>+5</button>
+                <button onClick={() => handleChangeLife(10)} style={{backgroundColor: 'transparent', fontSize: 48, color: "black", border: "3px solid black", padding: "8px", borderRadius: "8px", width: "120px"}}>+10</button>
+            </div>
+        </div>
+    )
+}
+
+export default PlayerCard
